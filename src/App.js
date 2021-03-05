00704286d830969cee487b6d433965ef43be827f
Ruslan <russlik88@gmail.com>
2021-03-05 09:28:27 +0100

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// @ts-nocheck
+import React from 'react';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: false
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.log(resp)
+        this.setState({ data: resp })
+      })
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    const data = this.state.data;
+    // console.log(data);
+    return (
+      <div>
+        {
+          data ?
+            <div style={{
+              display: 'flex',
+              maxWidth: 1210,
+              margin: '0 auto',
+              flexWrap: 'wrap',
+              gap: 35
+            }}>
+              {data.map(element => {
+                return (
+
+                  <div key={element.id} style={{
+                    flexBasis: 380,
+                    textAlign: 'justify',
+                    boxShadow: '0px 0px 33px #b4b0e8',
+                    padding: 15
+                  }}>
+                    {element.title}
+                    <br />
+                    {element.body}
+                  </div>
+                )
+              })}
+            </div> : <p>Please wait...</p>
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
